859228dc2307974e4ca4fb08287491ffdd00b60a
weiweiwitch <weiwei.witch@gmail.com>
2015-05-11 14:10:59 +0800

    - 调整目录配置。

--- src/main/java/org/ariane/thirdlab/config/WebConfig.java
+++ src/main/java/org/ariane/thirdlab/config/WebConfig.java
@@ -13,12 +13,12 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//		registry.addResourceHandler("/ap/**").addResourceLocations("/.tmp/serve/", "/app/").setCachePeriod(0);
-//		registry.addResourceHandler("/p/**").addResourceLocations("/dist/").setCachePeriod(0);
-//		registry.addResourceHandler("/bower_components/**").addResourceLocations("/bower_components/")
-//				.setCachePeriod(0);
+		registry.addResourceHandler("/ap/**").addResourceLocations("/.tmp/serve/", "/app/").setCachePeriod(0);
+		registry.addResourceHandler("/p/**").addResourceLocations("/dist/").setCachePeriod(0);
+		registry.addResourceHandler("/bower_components/**").addResourceLocations("/bower_components/")
+				.setCachePeriod(0);
 		
-		registry.addResourceHandler("/**").addResourceLocations("/").setCachePeriod(0);
+//		registry.addResourceHandler("/**").addResourceLocations("/").setCachePeriod(0);
 	}
 
 }
