299423ff75e63e5eac346690da6caa4b95ea1113
weiweiwitch <weiwei.witch@gmail.com>
2018-06-15 10:15:52 +0800

    增加jenkins的pipeline构建

---
+++ ci/build_backend.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# 显示一些环境信息
+echo $PATH
+java -version
+echo $JAVA_HOME
+pwd
+ls -al
+
+# 构建游戏服
+sh gradlew clean assembleBootDist
+
+# 复制发布文件到文件服务器
+DISTRIBUTION_SERVER=192.168.11.160
+DISTRIBUTION_SERVER_USER=ariane
+
+mkdir -p ~/.ssh/
+ssh-keyscan -H ${DISTRIBUTION_SERVER} | tee -a ~/.ssh/known_hosts
+scp -i ${keyfile} ${WORKSPACE}/build/distributions/thirdlab.tar.gz \
+    ${DISTRIBUTION_SERVER_USER}@${DISTRIBUTION_SERVER}:/home/${DISTRIBUTION_SERVER_USER}/docker/nginx/thirdlab/
---
+++ ci/build_frontend.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+cd src/main/resources/react
+
+echo $PATH
+
+yarn install
+yarn run prod_build
